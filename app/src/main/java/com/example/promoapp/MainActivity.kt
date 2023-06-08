6e622445a83cd5d2bee76f7976df1238ea39051d
JonasHagstrom79 <73818160+JonasHagstrom79@users.noreply.github.com>
2023-06-08 06:54:08 +0200

    added clicklistener

--- app/src/main/java/com/example/promoapp/MainActivity.kt
+++ app/src/main/java/com/example/promoapp/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.promoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main) //You can ctrl+click to jump to that file
+
+        val previewButton: Button = findViewById(R.id.button_preview)
+
+        previewButton.setOnClickListener {
+
+            Toast.makeText(this, "Testing toast", Toast.LENGTH_LONG).show()
+        }
+
     }
 }
