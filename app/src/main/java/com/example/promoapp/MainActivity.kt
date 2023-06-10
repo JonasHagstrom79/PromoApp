bb14e2ee95e0d8ee7c8d3888c2d8fce928a8b6ce
JonasHagstrom79 <73818160+JonasHagstrom79@users.noreply.github.com>
2023-06-11 01:00:02 +0200

    downgraded kotlin to 1.7.0

--- app/src/main/java/com/example/promoapp/MainActivity.kt
+++ app/src/main/java/com/example/promoapp/MainActivity.kt
@@ -2,20 +2,52 @@ package com.example.promoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.CheckBox
+import android.widget.Spinner
 import android.widget.Toast
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
+
+    /*
+    Class variables
+     */
+    private var contactNameEditText: TextInputEditText? = null
+    private var contactNumberEditText: TextInputEditText? = null
+    private var myDisplayNameEditText: TextInputEditText? = null
+    private var startDateEditText: TextInputEditText? = null
+    private var juniorCheckBox: CheckBox? = null
+    private var immediateStartCheckBox: CheckBox? = null
+    private var jobTitleSpinner: Spinner? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main) //You can ctrl+click to jump to that file
-
+        findViews()
         val previewButton: Button = findViewById(R.id.button_preview)
 
         previewButton.setOnClickListener {
-
-            Toast.makeText(this, "Testing toast", Toast.LENGTH_LONG).show()
+            onPreviewClicker() //Alt+Enter -> Create function
+            //Toast.makeText(this, "Testing toast", Toast.LENGTH_LONG).show()
         }
 
+    }
+
+    private fun findViews() {
+        contactNameEditText = findViewById(R.id.edit_text_contact_name)
+        contactNumberEditText = findViewById(R.id.edit_text_contact_number)
+        myDisplayNameEditText = findViewById(R.id.edit_text_my_display_name)
+        startDateEditText = findViewById(R.id.edit_text_start_date)
+        juniorCheckBox = findViewById(R.id.check_box_junior)
+        immediateStartCheckBox = findViewById(R.id.check_box_immediate_start)
+        jobTitleSpinner = findViewById(R.id.spinner_job_title)
+    }
+
+    private fun onPreviewClicker() {
+        Log.d("RandomTAG", "contactNameEditText" + contactNameEditText.toString())
+        val testString = contactNameEditText!!.text.toString() + ", " + contactNumberEditText!!.text.toString()
+        Toast.makeText(this, testString, Toast.LENGTH_LONG).show()
     }
 }
